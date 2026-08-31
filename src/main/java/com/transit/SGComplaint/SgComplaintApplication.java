2752400e68dadcdadee10eee9842b5c15cd5503b
leesub0719 <s6hnyy27h2@privaterelay.appleid.com>
2026-08-31 11:28:28 +0900

    Add src/main/java/com/transit/SGComplaint files

---
+++ src/main/java/com/transit/SGComplaint/SgComplaintApplication.java
@@ -0,0 +1,12 @@
+package com.transit.SGComplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgComplaintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgComplaintApplication.class, args);
+    }
+}
